aa1394a6fb51f44bda06677e3ccb6903f13a4832
Kavish-Mathur <kavishmathur11402@gmail.com>
2024-03-23 21:32:53 +0530

    Day-23 Question Description Added

--- src/Day_23/Q1_Reverse_Linked_List.java
+++ src/Day_23/Q1_Reverse_Linked_List.java
@@ -30,6 +30,22 @@ class solution_of_D23Q1
 
 public class Q1_Reverse_Linked_List
 {
+    /*
+        Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+
+            Example 1:
+                Input: head = [1,2,3,4,5]
+                Output: [5,4,3,2,1]
+
+            Example 2:
+                Input: head = [1,2]
+                Output: [2,1]
+
+            Example 3:
+                Input: head = []
+                Output: []
+    */
     public static void main(String[] args)
     {
         ListNode l5 = new ListNode(5);
--- src/Day_23/Q2_Merge_Two_Sorted_Linked_Lists.java
+++ src/Day_23/Q2_Merge_Two_Sorted_Linked_Lists.java
@@ -42,6 +42,26 @@ class solution_of_D23Q2
 
 public class Q2_Merge_Two_Sorted_Linked_Lists
 {
+    /*
+        You are given the heads of two sorted linked lists list1 and list2.
+
+        Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+        Return the head of the merged linked list.
+
+
+            Example 1:
+                Input: list1 = [1,2,4], list2 = [1,3,4]
+                Output: [1,1,2,3,4,4]
+
+            Example 2:
+                Input: list1 = [], list2 = []
+                Output: []
+
+            Example 3:
+                Input: list1 = [], list2 = [0]
+                Output: [0]
+    */
     public static void main(String[] args)
     {
         ListNode l1e4 = new ListNode(10);
